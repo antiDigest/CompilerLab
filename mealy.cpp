7f1d6bffe2bf1a3727a1d9b09b3176563094d0fb
Antriksh <antriksh5235@gmail.com>
2015-08-28 01:44:30 +0530

    Mealy and moore added

--- mealy.cpp
+++ mealy.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <fstream>
-#include <cstring>
+#include "iostream"
+#include "fstream"
+#include "cstring"
 
 using namespace std;
 
@@ -11,9 +11,9 @@ int main(){
 	int fs;
 	int regex[10][10];
 	int i,j, initial, final[10], k,m, flag=0, state, stin;
-	char inval[10];
+	char inval[100], outval[10][10], output[100];
 
-	ifstream in("input.txt");
+	ifstream in("inmealy.txt");
 	
 	in>>initial;
 	cout<<initial;
@@ -31,8 +31,10 @@ int main(){
 	m=i;
 
 	for (i=0;i<M;i++)
-		for (j=0;j<N;j++)
+		for (j=0;j<N;j++){
 			in>>regex[i][j];
+			in>>outval[10][10];
+		}
 
 	cout<<"Input String : \n";
 	cin>>inval;
@@ -41,12 +43,16 @@ int main(){
 	state = 0;
 	for(i=0;inval[i]!='\0';i++){
 		stin = inval[i] - 97;
+		output[i] = outval[state][stin];
 		state = regex[state][stin];
+		
 		if (state==-1){
 			cout<<"Invalid code input. Breaking Code.";
 			return 0;
 		}
 	}
+
+	k=i;
 
 	for(i=0;i<m;i++){
 		if(state == final[i]){
@@ -57,9 +63,14 @@ int main(){
 
 	if (flag==1)
 	{
-		printf("This is a match !\n");
+		cout<<"\nOutput : ";
+		for (int i = 0; i < k; i++)
+		{
+			cout<<output[i];
+		}
 	}
 	else{
 		printf("This is no match !\n");
 	}
+	return 0;
 }
--- moore.cpp
+++ moore.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <fstream>
-#include <cstring>
+#include "iostream"
+#include "fstream"
+#include "cstring"
 
 using namespace std;
 
@@ -11,9 +11,9 @@ int main(){
 	int fs;
 	int regex[10][10];
 	int i,j, initial, final[10], k,m, flag=0, state, stin;
-	char inval[10];
+	char inval[100], outval[10], output[100];
 
-	ifstream in("input.txt");
+	ifstream in("inmoore.txt");
 	
 	in>>initial;
 	cout<<initial;
@@ -30,9 +30,13 @@ int main(){
 
 	m=i;
 
-	for (i=0;i<M;i++)
-		for (j=0;j<N;j++)
+	for (i=0;i<M;i++){
+		in>>outval[10][10];
+		for (j=0;j<N;j++){
 			in>>regex[i][j];
+			
+		}
+	}
 
 	cout<<"Input String : \n";
 	cin>>inval;
@@ -41,12 +45,16 @@ int main(){
 	state = 0;
 	for(i=0;inval[i]!='\0';i++){
 		stin = inval[i] - 97;
+		output[i] = outval[state];
 		state = regex[state][stin];
+		
 		if (state==-1){
 			cout<<"Invalid code input. Breaking Code.";
 			return 0;
 		}
 	}
+
+	k=i;
 
 	for(i=0;i<m;i++){
 		if(state == final[i]){
@@ -57,9 +65,14 @@ int main(){
 
 	if (flag==1)
 	{
-		printf("This is a match !\n");
+		cout<<"\nOutput : ";
+		for (int i = 0; i < k; i++)
+		{
+			cout<<output[i];
+		}
 	}
 	else{
 		printf("This is no match !\n");
 	}
+	return 0;
 }
