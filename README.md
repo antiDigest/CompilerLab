8476d06e3da6cac05fddff9c5b5add76a37ac88e
Antriksh Agarwal <antriksh5235@gmail.com>
2015-09-07 22:59:54 +0530

    limits to the execution of the program

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # CompilerLab
 Compiler Lab
 
+##Important Note for QUESTION 3:
+
+The program works on C++11 which can be run on CodeBlocks as :
+
+*	Goto Settings -> Compiler
+
+*	The selected compiler should be set to "GNU GCC Compiler"
+
+*	In the "compiler settings" tab on the same window and the "Have g++ follow the C++11 ISO C++ language Standard [-std=c++11]"
+
+*	The program will now run perfectly
+
 ##Questions
 
 ![image1](images/img1.jpg)
--- nfa2dfa.cpp
+++ nfa2dfa.cpp
@@ -8,12 +8,19 @@
 
 using namespace std;
 
-#define M 4
 #define N 2
+int M = 4;
+
+int** intersection(int regex[10][10][10], int m, int n, int val[10]){
+    int i, j,k,l;
+    for (i=0;val[i]!=-2;i++){
+        cout<<val[i];
+    }
+}
 
 int main(){
 	int fs, kali;
-	int regex[10][10][10];
+	int regex[10][10][10], newreg[10][10], v[10];
 	int i,j, initial, final[10], k,m, flag=0, state, stin,l;
 	char inval[10];
 	char val[100];
@@ -38,6 +45,7 @@ int main(){
 		i++;
 	}
 
+
 	m=i;
     k=0;
 	for (i=0;i<M;i++)
@@ -57,10 +65,25 @@ int main(){
             }
         }
 
+
     for (i=0;i<M;i++){
         for (j=0;j<N;j++){
-            for (k=0;regex[i][j][k]!=-2;k++)
-                cout<<regex[i][j][k]<<",";
+            for (k=0;regex[i][j][k]!=-2;k++){
+                if (k>0){
+                    cout<<k;
+                    M = M+1;
+                    l=0;
+                    while(regex[i][j][l]!=-2){
+                        v[l] = regex[i][j][l];
+                        l++;
+                    }
+                    regex[i][j][0] = M;
+                    intersection(regex, i, j, v);
+                }
+                else{
+                    newreg[i][j] = regex[i][j][k];
+                }
+            }
             cout<<" ";
         }
         cout<<endl;
