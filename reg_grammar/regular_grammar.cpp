3119c14ad66f0f5b2346d70dc5950813a1d242de
evamy <antriksh5235@gmail.com>
2015-12-20 00:37:14 +0530

    properly sorted into folders, Regex, nfa2dfa and Regular Grammar not working yet

---
+++ reg_grammar/regular_grammar.cpp
@@ -0,0 +1,11 @@
+// WAP to read a regular grammar. Check whether a string given is acceptable or not.
+
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <fstream>
+#include <cstdio>
+
+int main(){
+	
+}
