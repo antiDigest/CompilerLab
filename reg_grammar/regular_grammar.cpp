375d2ef130884b7563def25d6cfba41870bb6ce1
evamy <antriksh5235@gmail.com>
2015-12-23 11:43:08 +0530

    Regular grammar changed, yet not implemented

--- reg_grammar/regular_grammar.cpp
+++ reg_grammar/regular_grammar.cpp
@@ -6,6 +6,44 @@
 #include <fstream>
 #include <cstdio>
 
+#define INPUT "input_grammar.txt"
+
+int n=0;
+
+struct prod
+{
+    char lhs[10],rhs[10][10];
+    int n;
+}pro[10];
+
 int main(){
-	
+
+	FILE *f;
+
+    for(int i=0;i<10;i++)
+        pro[i].n=0;
+
+    f=fopen(INPUT,"r");
+    while(!feof(f))
+    {
+        fscanf(f,"%s",pro[n].lhs);
+        if(n>0)
+        {
+            if( strcmp(pro[n].lhs,pro[n-1].lhs) == 0 )
+            {
+                pro[n].lhs[0]='\0';
+                fscanf(f,"%s",pro[n-1].rhs[pro[n-1].n]);
+                pro[n-1].n++;
+                continue;
+            }
+        }
+        fscanf(f,"%s",pro[n].rhs[pro[n].n]);
+        pro[n].n++;
+        n++;
+    }
+
+    printf("\n\nTHE GRAMMAR IS AS FOLLOWS\n\n");
+    for(int i=0;i<n;i++)
+        for(int j=0;j<pro[i].n;j++)
+            printf("%s -> %s\n",pro[i].lhs,pro[i].rhs[j]);
 }
